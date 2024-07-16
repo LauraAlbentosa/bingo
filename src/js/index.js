fdb5c18920938da5c2f4d28cbff468f1ab0156dc
lauraosallor <lauraosallor@gmail.com>
2024-07-16 09:04:49 +0200

    avances - POR CORREGIR

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,13 @@ import '../scss/styles.scss';
 const userCardElement = document.getElementById('user-card');
 const pcCardElement = document.getElementById('pc-card');
 const bingoCardElement = document.getElementById('bingo-card');
-const numberTextElement = document.getElementById('bingo-number')
-const startButonElement = document.getElementById('start-button')
+const numberTextElement = document.getElementById('bingo-number');
+const startButonElement = document.getElementById('start-button');
 
 let playerNumbers = [];
 let pcNumbers = [];
 let bingoNumbers = [];
+let bingoNumbersSelection = [];
 
 const createCards = () => {
   const fragment_user = document.createDocumentFragment();
@@ -37,7 +38,7 @@ const createCards = () => {
     divPlayer.append(spanPlayer);
     divPc.append(spanPc);
 
-    playerNumbers.push(numberCardPc);
+    playerNumbers.push(numberCardPlayer);
     pcNumbers.push(numberCardPc);
   }
 
@@ -47,6 +48,32 @@ const createCards = () => {
   userCardElement.append(fragment_user);
   pcCardElement.append(fragment_pc);
 };
+
+const createBoard = (array, board) =>{
+  const fragment = document.createDocumentFragment()
+  const div = document.createElement('div')
+
+  div.classList.add('card')
+
+  while(array.length < 16){
+    const number = Math.floor(Math.random()*99)
+    if(!array.includes(number)){
+      array.push(number)
+
+      const span = document.createElement('span')
+      
+      span.textContent = number
+
+      span.classList.add('box-player')
+
+      div.append(span)
+
+    }
+  }
+
+  fragment.append(div)
+  board.append(fragment)
+}
 
 const createBingoCard = () => {
   const fragment = document.createDocumentFragment();
@@ -66,22 +93,40 @@ const createBingoCard = () => {
   bingoCardElement.append(fragment);
 };
 
+const selectBingoNumbers = () => {
+  const spansUser = document.querySelectorAll('.user-card .card .box-players');
+  //console.log(playerNumbers)
 
+  for (let i = 1; i < 99; i++) {
+    const number = Math.floor(Math.random() * 99);
+    //console.log(number + ' ' +playerNumbers)
+    if (playerNumbers.includes(number)) {
+      const index = playerNumbers.indexOf(number);
+      spansUser[index].classList.add('green');
+      //console.log(number + ' entra')
+    }else{
+      //console.log(number + ' no entra')
+    }
+    //bingoNumbersSelection.push(number);
+  }
 
+  //console.log(bingoNumbersSelection);
+  // const interval = setInterval(() => {}, 500);
+};
 
-const selectBingoNumbers = () =>{
-    const interval = setInterval(()=>{
-       for 
+const checkPlayersCards = () => {
+  const spansUser = document.querySelectorAll('.user-card .card .box-players');
+  //console.log(spansUser)
 
-    }, 500)
+  for (const span of spansUser) {
+    console.log(span.textContent);
+  }
+};
 
+const start = () => {
+  createCards();
+  createBingoCard();
+  selectBingoNumbers();
+};
 
-}
-
-
-
-
-createCards();
-createBingoCard()
-
-startButonElement.addEventListener('click', selectBingoNumbers)
+startButonElement.addEventListener('click', start);
